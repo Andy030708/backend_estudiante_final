1b8bad8438212e3f31180777725a56730d6f0cc9
ANDY03 <andybarrosocastellon@gmail.com>
2026-02-05 16:10:48 -0500

    heatlh

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("estudiantes.urls")),
-    path("health/", include("health_check.urls")),
 ]
